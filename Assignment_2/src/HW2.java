826eab9e1031a0ce902012041a1c94f9c0c6121a
chihchii0209 <410313016@gms.ndhu.edu.tw>
2017-12-12 22:04:14 +0800

    前面進度消失了 所以重新照之前進度上傳一次 ... BEE

--- Assignment_2/src/HW2.java
+++ Assignment_2/src/HW2.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
@@ -5,30 +10,46 @@ import javax.sound.sampled.SourceDataLine;
 
 public class HW2 {
 
-	public static void main(String[] args) throws LineUnavailableException {
+	public static void main(String[] args) throws LineUnavailableException, IOException {
 		// TODO Auto-generated method stub
 		byte[] buf = new byte[ 1 ];
 		
-		float t = 2000 ;
-		float f = 880 ;
+		float FS = 2000 ;
 		
-	    AudioFormat af = new AudioFormat( t , 8, 1, true, false );
+		float T = 5 ;
+		
+		float[] f4 = {392, 330, 330, 0,
+					349, 294, 294, 0, 
+					262, 294, 330, 349,
+					392, 392, 392} ;
+		
+	    AudioFormat af = new AudioFormat( FS , 8, 1, true, false );
 	    SourceDataLine sdl = AudioSystem.getSourceDataLine( af );
+	    
+	    File dstFile = new File("C:/Users/chih chi/Desktop/sample.wav");
+		FileOutputStream out = new FileOutputStream(dstFile);
+		
+	    
 	    sdl.open();
 	    sdl.start();
 	    
 	    
-	    for( int i = 0 ; i < 1000 * t  / 1000 ; i++ ) {
+	    for(int j =0 ; j<15 ; j++){
+	    	for( int i = 0 ; i < T * FS /10 ; i++ ) {
 	    	
-	        double angle = i / ( t  / f ) * 2.0 * Math.PI;
-	        buf[ 0 ] = (byte )( Math.sin( angle ) * 100 );
-	        
-	        sdl.write( buf, 0, 1 );
+	    		double angle = (i / FS)  * f4[j]  * 2.0 * Math.PI ;
+
+	    		buf[ 0 ] = (byte )( Math.sin(angle) * 100 );
+	    	
+
+	    		sdl.write( buf, 0, 1 );
+	    		out.write(buf, 0, 1);
+	    	}
 	    }
-	    
 	    
 	    sdl.drain();
 	    sdl.stop();
+	    out.close();
 	}
 
 }
