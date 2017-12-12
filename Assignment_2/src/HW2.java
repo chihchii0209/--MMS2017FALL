f9799cd2536f8776d39aa9da1bfa703803287eac
chihchii0209 <410313016@gms.ndhu.edu.tw>
2017-12-13 00:22:43 +0800

    +input

--- Assignment_2/src/HW2.java
+++ Assignment_2/src/HW2.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -12,26 +13,43 @@ public class HW2 {
 
 	public static void main(String[] args) throws LineUnavailableException, IOException {
 		// TODO Auto-generated method stub
+		
+		String input ;
+		Scanner scan = new Scanner(System.in);
+		input = scan.nextLine() ;
+		
 		byte[] buf = new byte[ 1 ];
 		
 		float FS = 2000 ;
 		
+		float[] f4 = {262, 294, 330, 
+					349, 392, 440, 494} ;
+		
+		float[] f2 = {65, 73, 82,
+					87, 98, 110, 124} ;
+		
+		float[] sample = new float[50];
+		float[] sample2 = new float[50] ;
 		float T = 5 ;
 		
-		float[] f4 = {392, 330, 330, 0,
-					349, 294, 294, 0, 
-					262, 294, 330, 349,
-					392, 392, 392} ;
-		
-		float[] f2 = {98, 82, 82, 0,
-				87, 73, 73, 0,
-				65, 73, 82, 87,
-				98, 98, 98} ;
+		for(int i=0 ; i<input.length() ; i++){
+			if(input.charAt(i)==' '){
+				sample[i] = 0 ;
+				sample2[i] = 0 ;
+			}
+			else{
+				int index = input.charAt(i)-'0' ;
+				sample[i] = f4[index-1] ;
+				sample2[i] = f2[index-1] ;
+				
+			}
+		}
+
 		
 	    AudioFormat af = new AudioFormat( FS , 8, 1, true, false );
 	    SourceDataLine sdl = AudioSystem.getSourceDataLine( af );
 	    
-	    File dstFile = new File("C:/Users/chih chi/Desktop/sample.wav");
+	    File dstFile = new File("C:/Users/chih chi/Desktop/sample.mp3");
 		FileOutputStream out = new FileOutputStream(dstFile);
 		
 	    
@@ -39,13 +57,12 @@ public class HW2 {
 	    sdl.start();
 	    
 	    
-	    for(int j =0 ; j<15 ; j++){
+	    for(int j =0 ; j<input.length() ; j++){
 	    	for( int i = 0 ; i < T * FS /10 ; i++ ) {
 	    	
-	    		double angle = (i / FS)  * (f2[j] + f4[j])/2  * 2.0 * Math.PI ;
-	    		
+	    		double angle = (i / FS)  * (sample[j]+sample2[j])/2  * 2.0 * Math.PI ;
 
-	    		buf[ 0 ] = (byte )( Math.sin(angle) * 100 );
+	    		buf[ 0 ] = (byte )((Math.sin(angle)*(Math.sin(angle)) * 100 ));
 	    	
 
 	    		sdl.write( buf, 0, 1 );
@@ -55,6 +72,8 @@ public class HW2 {
 	    
 	    sdl.drain();
 	    sdl.stop();
+	    out.close();
+	    
 	}
 
 }
