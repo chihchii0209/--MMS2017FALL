4b5b17d6028cb295d48515781ef3204e78205077
chihchii0209 <410313016@gms.ndhu.edu.tw>
2018-01-28 02:50:34 +0800

    1/27

--- Assignment_3/src/HW3.java
+++ Assignment_3/src/HW3.java
@@ -1,54 +1,70 @@
 import java.io.IOException;
  import org.opencv.imgcodecs.*;
  import org.opencv.core.Core;
- import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
  import org.opencv.imgproc.Imgproc;
 
-
+ 
+ 
  public class HW3 {
 	 
-	 static Mat Rimg = Imgcodecs.imread("i1.pgm");
-	 static Mat Timg = Imgcodecs.imread("i2.pgm"); 
-	 
-	 org.opencv.core.Size img_size = Rimg.size();
-	 
-    public static void main(String[] args) {
-	 
-	 org.opencv.core.Size img_size = Rimg.size();
-	 
-    	int p = 16,u,v ;
-    	double min_MAD=9999 ;
+	static final int N = 16;
+	static final int p = 15; 
+	
+	public static void main(String[] args) throws IOException {
+        
+	System.loadLibrary(Core.NATIVE_LIBRARY_NAME); 
+	
+    Mat Rimg = Imgcodecs.imread("i1.jpg"); //Reference
+   	Mat Timg = Imgcodecs.imread("i2.jpg"); //Target
+   	Mat Clone_Rimg = Rimg.clone() ;
+ 
+    	for(int mX = 0; mX < 240; mX=mX+N) {
+            for(int mY = 0; mY < 320; mY=mY+N) { 
+                double min_MAD = Double.MAX_VALUE;
+                int u = 0, v = 0;
+
+                for(int i = -p; i <= p; i++) {
+                    for(int j = -p; j <= p; j++) {
+                       
+                        if(mX+i<0 || mX+i+N>240 || mY+j<0 || mY+j+N>320){
+                            continue;
+                        }
+                        double cur_MAD = MAD(mX, mY, i, j, Rimg, Timg);
+                        if(cur_MAD < min_MAD) {
+                            min_MAD = cur_MAD;
+                            u = i;
+                            v = j;
+                        }
+                    }
+                }
+
+                for(int i = 0; i < N; i++) {
+                    for(int j = 0; j < N; j++) {
+                    	Clone_Rimg.put(mX+i,mY+j, Rimg.get(mX+i+u, mY+j+v));
+                    }
+                }
+            }
+        }
     	
-    	for(int w=0 ; w<img_size.width ; w=w+(2*p)+1)
-    		for(int h=0 ; h<img_size.height ; h=h+(2*p)+1)
-    			
-    	 for(int i=-p ; i<p ; i++)
-    		 for(int j=-p ; j<p ; j++){
-    			 double cur_MAD = MAD(i,j) ;
-    			 if(cur_MAD < min_MAD){
-    				 min_MAD = cur_MAD ;
-    				 u=i ;
-    				 v=j ;
-    			 }
-    		 }
+    	Imgcodecs.imwrite( "C:\\Users\\chih chi\\Desktop\\Res.jpg", Clone_Rimg);
     	 
     	 
     	 
      }
 	
-	public static double MAD(int i, int j){
-		int N=16 ;
-		
-		for(int k=0 ; k<N-1 ; k++){
-			for(int l=0 ; l<N-1 ; l++){
-				
-				double[] Rpix = Rimg.get(x, y) ;
-				double[] Tpix = Timg.get(x, y) ;
-				
-				
-			}
-		}
-	}
+    static double MAD(int x, int y, int i, int j, Mat Rimg, Mat Timg) {
+        double temp = 0.0f;
+        for(int k = 0; k < N; k++) {
+            for(int l = 0; l < N; l++) {
+                temp += Math.abs(Timg.get(x+k, y+l)[0] - Rimg.get(x+i+k, y+j+l)[0]);
+            }
+        }
+        return temp/(double)(N*N);
+    }
+    
+    
 
  }
  
